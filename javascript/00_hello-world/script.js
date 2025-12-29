f0f18445de9ab9f527b0db94287b462f92fbeb72
rishabh <rishabhsurya55@gmail.com>
2025-12-30 02:08:27 +0530

    Add html course files

---
+++ javascript/00_hello-world/script.js
@@ -0,0 +1,6 @@
+const message =  'Hello world'
+const initialMessage = '   i am rishabh   '
+const faltu = '   i am rishabh    '.trim().toUpperCase()
+const final = 'i am rishabh'.replace('RISHABH', 'smart')
+
+
