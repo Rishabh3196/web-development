f43732cc678814e1d61acc8525e2d9ea3a2ef2ed
rishabh <rishabhsurya55@gmail.com>
2025-12-31 01:03:26 +0530

    operator

---
+++ javascript/08_maths-object/script.js
@@ -0,0 +1,8 @@
+const width = prompt("pleae enter width of rectengle: ")
+const length = prompt("pleae enter length of rectengle: ")
+
+console.log(length * width);
+console.log(2(length + width));
+
+document.writeln(`rectengle area: ${length * width}`);
+document.writeln(`rectengle perimeter: ${2(length + width)}`);
